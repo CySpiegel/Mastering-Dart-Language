60328bc60e91a382d2b6d4e16cdcfbb53dcf6d57
Matthew Stroble <chimiara999@msn.com>
2024-09-01 08:52:42 -0600

    loop tests

--- mastering_dart_language/lib/loops.dart
+++ mastering_dart_language/lib/loops.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 num sumOfNumbers(List mylist) {
   num sum = 0;
   for (var item in mylist) {
@@ -23,4 +25,33 @@ List foreEachInMap(Map mymap) {
   });
 
   return keyList;
+}
+
+List itterableLoopList(List myList) {
+  List printedList = [];
+  for (final item in myList) {
+    printedList.add(item);
+    print(item);
+  }
+  return printedList;
+}
+
+List incramentLoopList(List myList) {
+  List printedList = [];
+  for (var i = 0; i < myList.length; i++) {
+    printedList.add(myList[i]);
+    print(myList[i]);
+  }
+  return printedList;
+}
+
+List onlyEvenLoopList(List myList) {
+  List evenList = [];
+
+  for (final item in myList) {
+    if (item % 2 == 0) {
+      evenList.add(item);
+    }
+  }
+  return evenList;
 }
--- mastering_dart_language/test/loops_test.dart
+++ mastering_dart_language/test/loops_test.dart
@@ -15,4 +15,22 @@ void main() {
     expect(
         foreEachInMap({"1": 1, "2": 2, "3": 3, "4": 4}), ["1", "2", "3", "4"]);
   });
+
+  test('incramentLoopList', () {
+    expect(incramentLoopList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  test('iterableLoopList', () {
+    expect(itterableLoopList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+  test('incramentLoopList', () {
+    expect(incramentLoopList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  test('onlyEvenLoopList', () {
+    expect(onlyEvenLoopList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [2, 4, 6, 8, 10]);
+  });
 }
